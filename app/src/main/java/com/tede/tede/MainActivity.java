a669c92234564ddd3959f9d058cb11c5c0304fa8
ql0-0lp <mail@4e1.ru>
2023-05-27 07:16:30 +0300

    List of institutes buttons

--- app/src/main/java/com/tede/tede/MainActivity.java
+++ app/src/main/java/com/tede/tede/MainActivity.java
@@ -7,24 +7,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<Button> buttons;
+    private static final int[] BUTTON_IDS = {
+            R.id.button_iit,
+            R.id.button_iii,
+            R.id.button_ikb,
+            R.id.button_iptip,
+            R.id.button_iri,
+            R.id.button_itu,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button_iit);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent switcher = new Intent(MainActivity.this, InstituteTeacherActivity.class);
-                MainActivity.this.startActivity(switcher);
-            }
-        });
-
-
+        buttons = new ArrayList<Button>();
+        for(int id : BUTTON_IDS) {
+            Button button = (Button)findViewById(id);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent switcher = new Intent(MainActivity.this, InstituteTeacherActivity.class);
+                    startActivity(switcher);
+                }
+            });
+            buttons.add(button);
+        }
     }
 }
